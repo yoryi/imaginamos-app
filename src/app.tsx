38ba2fe0ea02b36d33018aa0c7fa17e49b90b042
yoryi <yoryiojeda@outlook.com>
2022-12-04 01:38:16 -0500

    fixed: ts app structure

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,12 @@
 //Developer: Jorginho Ojeda
 
 import React from 'react';
+import {LogBox} from 'react-native';
 import Router from './navegation';
 import {store} from './redux/store';
 import {Provider} from 'react-redux';
 
+LogBox.ignoreLogs(['createSlice.extraReducers']);
 type Props = {};
 const App: React.FC<Props> = () => {
   return (
--- src/services/api/config.tsx
+++ src/services/api/config.tsx
@@ -2,9 +2,7 @@ import axios from 'axios';
 import APP from '../../components/_settings/app';
 const config = axios.create({
   baseURL: APP.URI_API,
-  params: {
-    api_key: APP.KEY_API,
-  },
+  params: {api_key: APP.KEY_API},
   headers: {},
 });
 export default config;
